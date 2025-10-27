112694d19a7dbe2ab24718facedf12ef5ef053ac
DanichelsonHD <danichelson.hd@gmail.com>
2025-10-27 17:38:58 -0300

    Add initial ASP.NET Core MVC project structure
    Created a new ASP.NET Core MVC project with HomeController, ErrorViewModel, Startup, Program, and default views. Added configuration files, static assets, and project file for SalesWebMvc. This sets up the basic structure for further development.

---
+++ Exercicios/secao-18/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting; // <-- Importante: IHostEnvironment e IsDevelopment()
+
+namespace SalesWebMvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Configura os serviços da aplicação
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Mantém o comportamento clássico do curso (UseMvc)
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // Configura o pipeline de requisições
+        // Note que aqui usamos IHostEnvironment (Microsoft.Extensions.Hosting)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
